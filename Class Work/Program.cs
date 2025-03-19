caa8fa44911377994f18a3f70a7d1e4e6968270a
ravindudilhara99 <rdrajapaksha67@gmail.com>
2025-03-19 15:54:53 +0530

    Initial commit

---
+++ Class Work/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Work
+{
+    public class animal
+    {
+        //property - global scope
+        public int NoofLegs;
+
+        //int hairs;//automatic assigned as a private property
+
+        //main function - local scope
+
+        //syntax of a method
+        //access specifier return type method name(parameters)
+
+        public void Sound(string sound)
+        {
+            Console.WriteLine("The sound of the animal is " + sound);
+        }
+
+    }
+}
